a5d28452bf399a3958fbffbeb1f5bfffd06d2660
Mateus-Belfort <mateusaguia96@gmail.com>
2022-12-05 19:45:35 -0300

    incremento de icones funcionais

--- assets/index.js
+++ assets/index.js
@@ -26,6 +26,47 @@ function registrar() {
 }
 
 
+function resetar() {
+  window.location.reload();
+}
+
+
+function copiar() {
+  let btn = document.querySelector('#tooltip-copy');
+  btn.addEventListener('click', function (e) {
+    let textArea = document.querySelector('#textArea');
+    textArea.select();
+    document.execCommand('copy');
+    alert("Texto Copiado com sucesso!")
+  });
+}
+
+
+
+function imprimir() {
+  //pega o Html da DIV
+  var divElements = document.getElementById('textArea').innerHTML;
+  var contadas = document.getElementById('result').innerHTML;
+  if (divElements !== "") {
+    //pega o HTML de toda tag Body
+    var oldPage = document.body.innerHTML;
+
+    //Alterna o body 
+    document.body.innerHTML =
+      "<html><head><title></title></head><body>" +
+      divElements + "\n" + contadas + "</body>";
+
+    //Imprime o body atual
+    window.print();
+
+    //Retorna o conteudo original da página. 
+    document.body.innerHTML = oldPage;
+  } else {
+    alert("Registre algumas palavras para impressão!")
+  }
+
+}
+
 
 
 
